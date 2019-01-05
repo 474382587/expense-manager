ee24835beb42341a6522d4081d37a1f33359dba1
Joseph Jin <jerkjoe61@gmail.com>
2019-01-04 23:50:03 -0800

    date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`} ><h3>{description}</h3></Link>
-    <p>{amount} - {id}</p>
+    <p>{amount} - {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 )
 
